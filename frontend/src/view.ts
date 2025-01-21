3e93fc53b922d19920b373583370f707d6f6e7c3
DKormann <dominikormann@outlook.com>
2025-01-21 11:52:34 +0100

    pathing

--- frontend/src/view.ts
+++ frontend/src/view.ts
@@ -1,4 +1,5 @@
 
+// @ts-ignore
 import {Root as Root, getData, setData, Child, Path, root, child} from './data'
 import { htmlElement } from './_html'
 
@@ -21,11 +22,16 @@ type Line = {
   children:Page[]
 }
 
-type LinePath = [Path, number][]
-type PagePath = [...LinePath, [Path, null]]
 
-const linePath = (p:PagePath, l:number):LinePath => p.map(([p,n],_)=>[p,n===null?l:n])
-const pagePath = (l:LinePath, p:Path):PagePath =>[...l, [p, null]]
+type PagePath = [Path] | [Path, ...(Path|number)[], Path]
+type LinePath = [...PagePath, number]
+
+
+const linePath = (p:PagePath, l:number):LinePath => [...p, l]
+const pagePath = (l:LinePath, p:Path):PagePath =>[...l, p]
+
+// const parentLine = (p:PagePath) => p.slice(0,-1) as LinePath
+// const parentPage = (l:LinePath) => l.slice(0,-1) as PagePath 
 
 
 type State = {
@@ -38,7 +44,7 @@ const last = <T>(arr:T[]):T => arr[arr.length-1]
 const page = (r:Root, pagePath: PagePath):Page => {
   
   const pstring = JSON.stringify(pagePath)
-  const head:Head = {element:htmlElement("h2", datapath(pagePath).join('.'), ['id', pstring])}
+  const head:Head = {element:htmlElement("h2", (last(pagePath) as Path).join('.'), ['id', pstring])}
   const b = body(r, pagePath)
   const element = htmlElement("div", '', ['children',[head.element, b.element]])
   return {element, head, body:b, path:pagePath}
@@ -62,39 +68,56 @@ const line = (r:Root, path:LinePath, content:string, openLinks?: Path[]):Line =>
   return {element:lineElement(path, content, children), children}
 }
 
-const datapath=(p:LinePath|PagePath):Path=>last(p)[0]
+
 
 const body = (r:Root, path:PagePath):Body => {
-  const data = getData(r, datapath(path))  
+  const data = getData(r, last(path) as Path)  
   const lines = data.Content.split('\n').map((c,i)=>line(r, linePath(path, i), c, data.linkstate[i]))
   return {lines, element:htmlElement("div", '', ['children', lines.map(l=>l.element)], ['id', JSON.stringify(pagePath)])}
 }
 
-type Update = (s:State) => State
+
+// type Update = (s:State) => State
 type Jsonable = string|number|boolean|null|Jsonable[]|{[key:string]:Jsonable}
 
-const comp = <T extends Jsonable>(a:T, b:T):boolean => JSON.stringify(a) === JSON.stringify(b)
+const comp = <T extends Jsonable | undefined>(a:T, b:T):boolean => JSON.stringify(a) === JSON.stringify(b)
 
-const getPage = (s:State, path:PagePath):Page|undefined => {
-  return path.slice(0,-1).reduce((acc:Page|undefined, addr:[Path, number|null], i:number)=>{
-    const linnum = addr[1] as number
-    const pt = path[i+1][0]
-    return acc?.body.lines[linnum].children.find(ch=>comp(datapath(ch.path), pt))
-  }, s.page)
+const getPage = (s:State, path:PagePath) => {
+  const _getPage=(l:Line, path:PagePath):Page|undefined=>{
+    const [pt, nm, ...rest] = path
+    const pp = l.children.find(ch=>comp(last(ch.path), pt))
+    const ll = pp?.body.lines[nm as number]
+    return ll ? _getPage(ll, rest as PagePath) : pp
+  }
+  return _getPage({children:[s.page], element:document.body}, path)
 }
 
+// const setPage = (s:State, page:Page):State =>{
 
-const 
+//   const _setPage = (parent:Page, page:Page):Page =>{
+//     assertEq(parent.path.slice(0,-1), page.path.slice(0,parent.path.length-1), 'setPage path')
 
-const toggleLink = (linePath:LinePath, path:Path):Update => (s)=>{
-  return s
-}
+//     return {
+//       ...parent,
+//       // child: parent.childrend.concat(_setPage(
+//     }
+//   }
+
+//   return {...s, page:_setPage(s.page, page)}
+// }
+
+// const toggleLink = (linePath:LinePath, path:Path):Update => (s)=>{
+//   return s
+// }
 
 
 const assertEq = (a:Jsonable|undefined, b:Jsonable|undefined, message:string) => {
-  if (a === undefined || b === undefined) if (a !== b) console.error(message, a, b)
   if (!comp(a!,b!)) console.error(message, a, b)
 }
+
+// const assertNeq = (a:Jsonable|undefined, b:Jsonable|undefined, message:string) => {
+//   if (comp(a!,b!)) console.error(message, a, b)
+// }
 
 const pageText=(page:Page):string => page.body.lines.map(l=>l.element?.childNodes[0].textContent).join('\n')
 
@@ -102,14 +125,14 @@ const pageText=(page:Page):string => page.body.lines.map(l=>l.element?.childNode
   console.log("testing view")
   const r = root(child(["me"], "hello #note", [[['note']]]), child(["note"], "note."))
 
-  const s:State = {root:r, page:page(r, [[["me"], null]])}
-  assertEq(pageText(getPage(s, [[['me'], null]])!), 'hello #note', 'view test');
-  assertEq(pageText(getPage(s, [[['me'], 0], [['note'], null]])!), 'note.', 'view test');
+  const s:State = {root:r, page:page(r, [["me"]])}
+  assertEq(pageText(getPage(s, [['me']])!), 'hello #note', 'view test');
+  assertEq(pageText(getPage(s, [['me'],0, ['note']])!), 'note.', 'view test');
 }
 
 export const view = (show: (el:HTMLElement)=>void) => {
   const r = root(child(["me"], "hello #note", [[['note']]]), child(["note"], "note."))
-  const s:State = {root:r, page:page(r, [[["me"],null]])}
+  const s:State = {root:r, page:page(r, [["me"]])}
   show(
     htmlElement("div", '', ['children', [s.page.element]], 
       ['onclick', (e:MouseEvent)=>{
