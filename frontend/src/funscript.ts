989f51df0139407e8d9dd2887dab172662c25dc7
DKormann <dominikormann@outlook.com>
2025-02-18 16:22:49 +0100

    tertiary works

--- frontend/src/funscript.ts
+++ frontend/src/funscript.ts
@@ -1,93 +1,4 @@
-
-
-// FunScript: pure functional script language similar to JS
-
-// ## stuff that is like JS:
-
-// - arrow functions
-// - function calling
-// - numbers & strings
-// - arrays & objects
-// - if else shorthand with `? :`
-
-// ```javascript
-// console.log("Hello, world!")
-// ```
-
-// ```javascript
-// x = 22 + 33;
-// arr = [x, 1, 3];
-// obj = {a: 1, b: 2};
-// ```
-
-// ```javascript
-// arrow = (x) => x + 1;
-// max = (a, b) => a > b ? a : b;
-// ```
-
-// ## stuff we dont have:
-
-// - no `var`, `let`, `const` - all variables are immutable
-// - no `this` - all functions are pure
-// - no `for` loops - use recursion instead
-// - no `if` else - use ? : notation instead
-// - statements (!) - everything is an expression
-
-// ## stuff we have that JS doesn't:
-
-// - infix variable assigment - you can assign variables in the middle of any expression
-
-
-// ```javascript
-// obj = {a: 1, mx: 
-//   q = 33;
-//   q + 1;
-// }
-// ```
-
-// ```javascript
-// [
-//   x = get_x();
-//   y = get_y();
-//   x + y,
-
-//   z = get_z();
-//   z + 1
-// ] // this array will have to values: x + y and z + 1. x, y and z are local to the array not global
-// ```
-
-
-// ### tradeoffs:
-// - you dont need curly braces for anything except objects
-// - semicolons specifically for variable assignment
-// - code becomes dense but concise and fast to write
-
-
-// ### example:
-
-// ```javascript
-
-// // fibonacci
-
-// fib = n => 
-//   n < 2 ?
-//   n :
-//   fib(n - 1) + fib(n - 2);
-
-// _ = console.log(fib(10));
-
-// fast_fib = n => 
-//   _fib = (a, b, n) =>
-//     n == 0 ?
-//     a :
-//     _fib(b, a + b, n - 1);
-//   _fib(0, 1, n);
-
-// console.log(fast_fib(100))
-
 import { assertEq, assertErr, last, log, stringify, Res, Ok, Err, ok, err } from "./helpers"
-
-
 
 type code = {
   type: string,
@@ -98,14 +9,13 @@ type code = {
 
 type token = code & {type:"number" | "string" | "boolean" | "null" | "identifier" | "symbol" | "typo" | "whitespace" | "comment"}
 
-const symbols = ["(", ")", "{", "}", "[", "]", ",", ":", "?", "=>", "!", "&&", "||", "+", "-", "*", "/", "%", "<", ">", "<=", ">=", "==", "!=", "=", ";", "...", ".", "//"]
+const symbols = ["(", ")", "{", "}", "[", "]", "=>", ",", ":", "?", "=>", "!", "&&", "||", "+", "-", "*", "/", "%", "<", ">", "<=", ">=", "==", "!=", "=", ";", "...", ".", "//"]
 const symbol_letters = new Set(symbols.join('').split(''))
 
 const seek = (code:string, start:number, pred: (c:string, i:number)=>boolean):number =>{
   const off = code.slice(start).split('').findIndex(pred)
   return off == -1 ? code.length : start + off
 }
-
 
 const tokenize = (code:string, i:number=0, tid = 0):token[] =>{
   if (code.length <= i) return []
@@ -126,44 +36,52 @@ const tokenize = (code:string, i:number=0, tid = 0):token[] =>{
   return [{type:typ, value:code.slice(i, nxt), start:i, end:nxt}, ...tokenize(code, nxt, tid+1)]
 }
 
-
 assertEq(tokenize("1"), [{type:"number", value:"1", start:0, end:1}])
 assertEq(tokenize("1 +  1"), [{type:"number", value:"1", start:0, end:1}, {type:"whitespace", value:" ", start:1, end:2}, {type:"symbol", value:"+", start:2, end:3}, {type:"whitespace", value:"  ", start:3, end:5}, {type:"number", value:"1", start:5, end:6}])
 assertEq(tokenize('{"gello" + ]22', 0), [{type:"symbol", value:"{", start:0, end:1}, {type:"string", value:"\"gello\"", start:1, end:8}, {type:"whitespace", value:" ", start:8, end:9}, {type:"symbol", value:"+", start:9, end:10}, {type:"whitespace", value:" ", start:10, end:11}, {type:"symbol", value:"]", start:11, end:12}, {type:"number", value:"22", start:12, end:14}])
 assertEq(tokenize('true'), [{type:"boolean", value:"true", start:0, end:4}])
 assertEq(tokenize("a + // comment\nb"), [{type:"identifier", value:"a", start:0, end:1}, {type:"whitespace", value:" ", start:1, end:2}, {type:"symbol", value:"+", start:2, end:3}, {type:"whitespace", value:" ", start:3, end:4}, {type:"comment", value:"// comment", start:4, end:14}, {type:"whitespace", value:"\n", start:14, end:15}, {type:"identifier", value:"b", start:15, end:16}])
 
-
 type ast = nullary | unary | binary | ternary | nary
 
 type nullary = code & {type:"number" | "string" | "boolean" | "null" | "identifier" | "typo", children:[]}
 type unary = code & {type:"!" | "neg" | "..."} & {children: [ast]}
-type binary = code & {type:"+" | "-" | "*" | "/" | "%" | "<" | ">" | "<=" | ">=" | "==" | "!=" | "&&" | "||" | "app" | "idx" | "[]" | ":"} & {children: [ast, ast]}
+type binary = code & {type:"+" | "-" | "*" | "/" | "%" | "<" | ">" | "<=" | ">=" | "==" | "!=" | "&&" | "||" | "app" | "=>" | "idx" | "[]" | ":"} & {children: [ast, ast]}
 type ternary = code & {type:"?:" | "=;"} & {children: [ast, ast, ast]}
 type nary = code & {type:"{}" | "[]" | "()"} & {children: ast[]}
 
-
+const ternaryops = ["?:", "=;"]
 const symbolpairs = [["(", ")"], ["{", "}"], ["[", "]"], ["?", ":"], ["=", ";"]]
+const binaryops = ["+", "-", "*", "/", "%", "<", ">", "<=", ">=", "==", "!=", "&&", "||", "app", "=>", "idx", "[]"]
+const unaryops = ["!", "neg", "..."]
 
 
 const parse = (code:string): ast => {
 
   const tokens = tokenize(code)
-
   const nonw = (idx:number): number =>
     tokens[idx] == undefined? idx :tokens[idx].type == "whitespace" || tokens[idx].type == "comment" ? nonw(idx+1): idx
 
   const nexttok = (prev: code| ast):number =>nonw(tokens.findIndex(t=>t.start >= prev.end))
 
+  const parseKV = (idx:number):ast =>{
+    const k = parseexpr(idx)
+    const colon = tokens[nexttok(k)]
+    if (colon.value != ":") return k
+    const v = parseexpr(nexttok(colon))
+    return {type:":", value:"", start:k.start, end:v.end, children:[k, v]}
+  }
+
   const parsegroup = (opener:token , idx: number):nary => {
     const closer = symbolpairs.find(s=>s[0] == opener.value)?.[1]
-    const type = opener.value == "(" ? "()" : opener.value == "{" ? "{}" : "[]"
     if (closer == undefined) throw new Error("parsegroup error "+ opener.value+ " not an opener")
+      const type = opener.value + closer as (nary)["type"]
     const tok = tokens[idx]
     if (tok.value == closer) return {type, value:"", children:[], start: opener.start, end: tok.end}
-    if (tok.value == ",") return parsegroup(opener, nonw(idx+1))
+    if (tok.value == ",") return parsegroup(opener, nexttok(tok))
       
-    const child = parseexpr(idx)
+    const child = type == "{}" ? parseKV (idx) : parseexpr(idx)
+
     const rest = parsegroup(opener, nexttok(child))
     return {...rest, children:[child, ...rest.children]}
   }
@@ -191,24 +109,26 @@ const parse = (code:string): ast => {
     const nextop = tokens[nexttok(first)]
     if (nextop == undefined) return first
     if (nextop.type == "symbol"){
-
-      const op: ast["type"] = nextop.value == "(" ? "app" : nextop.value == "[" ? "idx" : nextop.value == "." ? "idx" : (nextop.value as ast['type'])
-      if (['+', '-', '*', '/', '%', '<', '>', '<=', '>=', '==', '!=', '&&', '||', ":"].includes(op)){
-        const second = parseexpr(nonw(nonw(idx+1)+1))
+      const op: ast["type"] = nextop.value == "(" ? "app" : nextop.value == "[" ? "idx" : nextop.value == "." ? "idx" :
+        (nextop.value == "?")? "?:": (nextop.value == "=")? "=;":
+        (nextop.value as ast['type'])
+      if (binaryops.includes(op)){
+        const second = parseexpr(nexttok(nextop))
         return {type: op, value: "", start: first.start, end:second.end, children: [first, second]} as binary
       }
+
+      if (ternaryops.includes(op)){
+        const grp = parsegroup(nextop, nexttok(nextop))
+        const els = parseexpr(nexttok(grp))
+        return {type:op, value:"", start:first.start, end:els.end, children:[first, grp.children[0], els]} as ternary
+
+      }
     }
-    
     return first
   }
 
   return parseexpr(0)
 }
-
-assertEq(parse("1"), {type:"number", value:"1", start:0, end:1, children:[]})
-assertEq(parse("2312"), {type:"number", value:"2312", start:0, end:4, children: []})
-assertEq(parse("true"), {type:"boolean", value:"true", start:0, end:4, children: []})
-assertEq(parse("{1}"), {type:"{}", value:"", start:0, end:3, children:[{type:"number", value:"1", start:1, end:2, children: []}]})
 
 
 const build = (ast:ast):string =>{
@@ -222,117 +142,118 @@ const build = (ast:ast):string =>{
   (ast.type == 'neg')? `-${build(ast.children[0])}`:
   ast.children.length == 2 ? sfill(`({}${ast.type}{})`, 0):
   ast.children.length == 1 ? `${ast.type}${build(ast.children[0])}`:
+  (ternaryops.includes(ast.type)) ? sfill(`({}${ast.type[0]}{}${ast.type[1]}{})`, 0):
   "not implemented: "+ast.type
 }
 
-assertEq(build(parse("1")), "1")
-assertEq(build(parse("{1}")), "{1}")
+
+const operator_weight = (op:ast['type']):number =>
+  (op =="?:")||(op === "=>") ? 6 :
+  (op === "<")||(op === ">")||(op === "<=")||(op === ">=")||(op === "==")||(op === "!=")?7:
+  (op === "&&")|| (op === "||")?9:
+  (op === "+")||(op === "-") ?10:
+  (op === "/")|| (op === "%")|| (op === "*")?11:
+  (unaryops.includes(op)) ? 12 :
+  (op === "app") || (op=="=;") ||(op=="...")?13 :
+  (op == "idx")? 14:
+  (op === "[]") || (op === "{}") || (op === "()")?15:
+  (!binaryops.includes(op)) ? 16:
+  -1
 
 
-const testbuild = (...codes:string[]) =>{
+const rearange = (nod:ast):ast => {
 
-  try{
+  const cmap = (nd:ast) => ({...nd, children:nd.children.map(rearange)} as ast)
+  const node = cmap(nod)
 
-    const built = build(parse(codes[0]))
-    assertEq(built, codes[1] ?? codes[0], " compiling "+ codes[0])
-  }catch(e){
-    console.error(e, " in compiling "+ codes[0])
+  if (node.type )
+  if (unaryops.includes(node.type)){
+    const [fst] = (node as unary).children
+    if (operator_weight(node.type) > operator_weight(fst.type)){
+      return cmap({...fst, children:[{...node, children:[fst.children[0] as ast]} , ...fst.children.slice(1)]} as ast)
+    }
   }
-  
-  
+  if (binaryops.includes(node.type)) {
+    const [fst, snd] = (node as binary).children
+    if (
+      operator_weight(node.type) > operator_weight(snd.type)){
+      const sb = snd as binary
+      return cmap({...sb, children:[{...node, children:[fst, sb.children[0]]}  as binary,  ...sb.children.slice(1)]} as ast)
+    }
+  }
+
+  return node
 }
+
+const compile =(s:string) => build(
+  // log
+  (rearange(parse(s)))
+)
+
+
+
+{
+
+  const testbuild = (...codes:string[]) =>{
+    try{
+      const built = build(parse(codes[0]))
+      assertEq(built, codes[1] ?? codes[0], " compiling "+ codes[0])
+    }catch(e){
+      console.error(e, " in compiling "+ codes[0])
+    }
+  }  
+  assertEq(parse("1"), {type:"number", value:"1", start:0, end:1, children:[]})
+  assertEq(parse("2312"), {type:"number", value:"2312", start:0, end:4, children: []})
+  assertEq(parse("true"), {type:"boolean", value:"true", start:0, end:4, children: []})
+  assertEq(parse("{1}"), {type:"{}", value:"", start:0, end:3, children:[{type:"number", value:"1", start:1, end:2, children: []}]})
+
+  assertEq(build(parse("1")), "1")
+  assertEq(build(parse("{1}")), "{1}")
   
-testbuild("1")
-testbuild("{1}")
-testbuild("true")
-testbuild("false")
-testbuild("null")
-testbuild("123")
-testbuild("{1, 2,3,}", "{1,2,3}")
-testbuild("{}")
-testbuild("{1,}", "{1}")
-testbuild("-x")
-testbuild("x + y", "(x+y)")
+  
+  testbuild("1")
+  testbuild("{1}")
+  testbuild("true")
+  testbuild("false")
+  testbuild("null")
+  testbuild("123")
+  testbuild("{1, 2,3,}", "{1,2,3}")
+  testbuild("{}")
+  testbuild("{1,}", "{1}")
+  testbuild("-x")
+  testbuild("x + y", "(x+y)")
 
-testbuild("a:b", "(a:b)")
-testbuild("[a+1]", "[(a+1)]")
-testbuild("[a+1,b-1]", "[(a+1),(b-1)]")
-testbuild("{a:1}", "{(a:1)}")
-testbuild("...a", "...a")
+  testbuild("[a+1]", "[(a+1)]")
+  testbuild("[a+1,b-1]", "[(a+1),(b-1)]")
+  testbuild("{a:1}", "{(a:1)}")
+  testbuild("...a", "...a")
 
-testbuild("{a:1, b:2,...c}", "{(a:1),(b:2),...c}")
-
-
+  testbuild("{a:1, b:2,...c}", "{(a:1),(b:2),...c}")
 
 
+  const testCompile = (code:string, expected: string)=>{
+    try{
+      assertEq(compile(code), expected, " in rearanging " + code)
+    }catch(e){
+      console.error(e, " in rearanging " + code)
+    }
+  }
+
+  testCompile("1", "1")
+  testCompile("a + 3", "(a+3)")
 
 
+  testCompile("!a+b", "(!a+b)")
 
-// const operator_weight = (op:astop):number =>
-//   (op =="?:")||(op === "=>") ? 6 :
-//   (op === "<")||(op === ">")||(op === "<=")||(op === ">=")||(op === "==")||(op === "!=")?7:
-//   (op === "&&")|| (op === "||")?9:
-//   (op === "+")||(op === "-") ?10:
-//   (op === "/")|| (op === "%")|| (op === "*")?11:
-//   op==="!" ?12:
-//   (op === "app") || (op=="=;") ||(op=="...")?13 :
-//   (op == "idx")? 14:
-//   (op === "[]") || (op === "{}") || (op === "arglist")?15:
-//   -1
+  testCompile("a + b * c", "(a+(b*c))")
+  testCompile("a * b + c", "((a*b)+c)")
+  testCompile("!a * b +c", "((!a*b)+c)")
+  testCompile("a ? b : c", "(a?b:c)")
 
-// const rearange = (node:astnode):astnode => {
+  testCompile("a>b?c:d", "((a>b)?c:d)")
+  testCompile("a=b;c", "(a=b;c)")
 
-//   const res = {...node, children:node.children.map(rearange)}
-
-//   if (node.type === "{}" || node.type === "[]" || node.type === "arglist") return res
-//   if (node.arity == 1){
-//     const fst = res.children[0]
-//     if (!fst.children.length || operator_weight(node.type) < operator_weight(fst.children[0].type)) return res 
-//     return {...fst,children:[newunary(node.type as unaryop, fst.children[0]), ...fst.children.slice(1)]}
-//   }
-//   if (node.arity == 2){
-//     const [fst, snd] = res.children
-//     if (snd.children.length < 2 || operator_weight(node.type) <= operator_weight(snd.type)) return res
-//     if (snd.type === "=>") return res
-//     return {
-//       type:snd.type,
-//       arity:snd.arity,
-//       children:[{
-//         type:node.type,
-//         arity:2,
-//         children:[fst, snd.children[0]]
-//       }, ...snd.children.slice(1)]
-//     }
-//   }
-//   return res as expression
-// }
-
-// const parse = (code:string):ParseResult =>
-//   naive_parse(code).and(x=>ok(rearange(x.val), x.idx))
-
-// const buildjs = (ast:astnode):string =>{
-
-//   const sfill = (template:string , ... children:astnode[]):string =>
-//     children.length == 0?template:
-//     sfill(template.replace("{}", buildjs(children[0])), ...children.slice(1));
-
-//   return ast.type == "number" || ast.type == "boolean" ? `${(ast as literal).value}`:
-//   ast.type == "string" ? `"${(ast as literal).value}"`:
-//   ast.type == "identifier" ? (ast as identifier).value:
-//   ast.type == ":" ? sfill(`{}:{}`, ...ast.children):
-//   ast.type == "{}" ? `{${ast.children.map(buildjs).join(",")}}`:
-//   ast.type == "[]" ? `[${ast.children.map(buildjs).join(",")}]`:
-//   ast.type == "idx" ? sfill(`({}[{}])`, ...ast.children):
-//   ast.type == "?:" ? sfill(`({}?{}:\n{})`, ...ast.children):
-//   ast.type == "=;" ? sfill(`(()=>{const {}={};\nreturn {}})()`, ...ast.children):
-//   ast.type == "arglist" ? `(${ast.children.map(buildjs).join(",")})`:
-//   ast.arity == 2 ? sfill(`({}${ast.type == "app"?"":ast.type}${
-//     ast.type == "=>" ? "({})" : "{}"
-//   })`, ...ast.children):
-//   ast.arity == 1 ? sfill(`${ast.type}{}`, ...ast.children):
-//   "<unknown>"
-// }
-
+}
 
 
 // const compile = (code:string):Result<string> => 
