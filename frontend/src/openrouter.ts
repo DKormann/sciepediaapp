1269852d3a6ce9d97a530e382ad07fc20f0822c5
Dominik Kormann <dominikormann@outlook.com>
2025-06-18 14:40:43 +0200

    openrouter wip

--- frontend/src/openrouter.ts
+++ frontend/src/openrouter.ts
@@ -11,25 +11,33 @@ const answer = (question:string)=>fetch('https://openrouter.ai/api/v1/chat/compl
     'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
     'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
     'Content-Type': 'application/json',
+
   },
   body: JSON.stringify({
     // model: 'qwen/qwq-32b:free',
     model: "deepseek/deepseek-r1-zero:free",
     messages: [
       {
+        role: 'system',
+        content: 'You are a helpful, honest, and respectful AI assistant. Focus on the user’s intent, and ask clarifying questions if needed.',
+      },
+      {
         role: 'user',
         content: question,
       },
+      {
+        role: 'assistant',
+        content: 'The capital of France',
+      }
     ],
   }),
 }).then(r=>r.json()).then(r=>r.choices[0].message);
 
 
-answer("whats the capital of france").then(d=>{
-  if (d.reasoning == undefined) return log("cant get answer right now")
-  log({reasoning: d.reasoning});
-  log({answer:d.content})
-})
+// answer("whats the capital of France").then(d=>{
+//   if (d.reasoning) log({reasoning: d.reasoning});
+//   log({answer:d.content})
+// })
 
 
 
